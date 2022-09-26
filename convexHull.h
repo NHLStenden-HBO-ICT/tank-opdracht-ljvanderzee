146553a189f382297ef18809aa1e168e14997741
Lolke Jelmer van der Zee <ljvanderzee@gmail.com>
2022-09-26 10:03:26 +0200

    Created a class for the convexhull code

--- convexHull.h
+++ convexHull.h
@@ -2,13 +2,20 @@
 
 namespace Tmpl8
 {
-	/*class convexHull
+	class Convexhull
 	{
 	public:
-		convexHull(vector<vec2>& activeTanksPositions);
-		void heapSort(vector<vec2>& activeTanksPositions, int sizeOfVector);
-		void minHeapify(vector<vec2>& tanks, int index, int sizeOfVector, vec2 origin);
+		Convexhull(vector<vec2>& activeTanksPositions);
+
+		~Convexhull();
+
+		void heapSort(vector<vec2>& activeTanksPositions, int sizeOfVector, vec2 origin);
+		void maxHeapify(vector<vec2>& tanks, int index, int sizeOfVector, vec2 origin);
+		bool compareAngles(vec2 p0, vec2 p1, vec2 p2);
+		int convex(const vec2& p0, const vec2& p1, const vec2& p2);
+		vector<vec2> getPointsOnHull();
 	private:
-	};*/
+		vector<vec2> pointsOnHull;
+	};
 }
 
